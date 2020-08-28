a95ff126a762d9634d6fcb01783ce4e31c728de7
Adrien <adrien.delpire@gmail.com>
2020-08-28 16:40:30 +0200

    mail

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
         <br>
         <br>
         <label for="email">email</br></label>
-        <input type="text" name="email" placeholder="aze@gmail.com">
+        <input type="email" name="email" placeholder="aze@gmail.com">
         <br>    
         <select name="pays" id="">
             <option value="pays" selected="selected">Pays </option>
@@ -318,7 +318,62 @@
                 }
             }
         ?>
+    <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
+require 'assets/php/PHPMailer/src/PHPMailer.php';
+require 'assets/php/PHPMailer/src/SMTP.php';
+require 'assets/php/PHPMailer/src/Exception.php';
+
+$lastname = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+    $firstname = filter_var($_POST['lastname'], FILTER_SANITIZE_STRING);
+    $gender = filter_var($_POST['sex'], FILTER_SANITIZE_STRING);
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+    $country = filter_var($_POST['pays'], FILTER_SANITIZE_STRING);
+    $subject = filter_var($_POST['choice'], FILTER_SANITIZE_STRING);
+    $message = filter_var($_POST['message'], FILTER_SANITIZE_STRING);
+
+    function sendEmail()
+    {
+        global $lastname;
+        global $firstname;
+        global $gender;
+        global $email;
+        global $country;
+        global $subject;
+        global $message;
+
+        $mail = new PHPMailer(true);
+        try {
+            //Server settings
+            $mail->SMTPDebug = 0;                                                                                    // Enable verbose debug output
+            $mail->isSMTP();                                                                                                // Send using SMTP
+            $mail->Host       = 'smtp.gmail.com';                                                          // Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                                                          // Enable SMTP authentication
+            $mail->Username   = 'ticket.hackerspoulette@gmail.com';         // SMTP username
+            $mail->Password   = 'Test12341234';                                                  // SMTP password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;  // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+            $mail->Port       = 587;                                                                           // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+            //Recipients
+            $mail->setFrom('ticket.hackerspoulette@gmail.com', 'Support');
+            $mail->addAddress($email);     // Add a recipient
+
+            // Content
+            $mail->isHTML(true);                                  // Set email format to HTML
+            $mail->Subject = 'Thank you for contacting us';
+            $mail->Body    = "<p>Hello <strong>" . $lastname ." " . $firstname . ".</strong></br> Thank you for contacting us.</br>We will reply to you as soon as possible. </br> Your ticket was about a " . $subject . " :</br> <i>" . $message . "</i></br>See you soon.</p>";
+
+            $mail->send();
+        } catch (Exception $e) {
+            echo "Can't send the support ticket . Error: {$mail->ErrorInfo}";
+        }
+    }
+
+    sendEmail();
+?>
     
 
 </body>
